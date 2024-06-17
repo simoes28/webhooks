8eb285267f06ed7a2e35f6234f39c767b091c3be
Victor Simões <simoes2812@gmail.com>
2024-06-17 09:37:28 -0300

    teste34

--- index.cjs
+++ index.cjs
@@ -13,7 +13,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-io.post('connection', (socket) => {
+app.post('connection', (socket) => {
     console.log('Cliente conectado', socket.id);
   
     // Evento para adicionar o cliente a uma sala
