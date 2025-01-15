e5101307d7aec4a75e47fe24a64af79640edd030
Victor Simões <simoes2812@gmail.com>
2025-01-15 12:22:34 -0300

    Atualização de teste

--- index.cjs
+++ index.cjs
@@ -1,235 +1,251 @@
 const express = require("express");
 const http = require("http");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
 
-io.on('connection', (socket) => {
-    console.log('Cliente conectado', socket.id);
-  
-    // Evento para adicionar o cliente a uma sala
-    socket.on('joinRoom', ({url, queueId})=>{
-        const room= `${url}_${queueId}`
-        socket.join(room);
-        console.log(`Cliente ${socket.id} entrou na sala: ${room}`);
-    })
+io.on("connection", (socket) => {
+  console.log("Cliente conectado", socket.id);
 
-    socket.on('disconnect', () => {
-      console.log('Cliente desconectado', socket.id);
-    });
+  // Evento para adicionar o cliente a uma sala
+  socket.on("joinRoom", ({ url, queueId }) => {
+    const room = `${url}_${queueId}`;
+    socket.join(room);
+    console.log(`Cliente ${socket.id} entrou na sala: ${room}`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado", socket.id);
+  });
 });
 
 app.use(express.json());
 
 //Captura webhooks:
-app.post("/newChat", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila
+app.post("/newChat", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-        io.to(room).emit("webhookNewChat", data);
-        res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`)
-        }else {
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
-        }
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookNewChat", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
     } else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-   
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
+app.post("/newMessage", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila, numero_cliente, mensagem, chat_id
 
-app.post("/newMessage", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila, numero_cliente, mensagem, chat_id
-
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookNewMessage", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`)
-        }else {
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
-        }
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookNewMessage", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
     } else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/chatClosedHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila
+app.post("/chatClosedHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookchatClosedHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookchatClosedHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/typingHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
+app.post("/typingHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhooktypingHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhooktypingHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/authStatusHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila
+app.post("/authStatusHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookauthStatusHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookauthStatusHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/msgReceivedByServerHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila
+app.post("/msgReceivedByServerHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookmsgReceivedByServerHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookmsgReceivedByServerHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/msgReceivedByUserHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila, chat_id
+app.post("/msgReceivedByUserHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila, chat_id
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookmsgReceivedByUserHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookmsgReceivedByUserHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/msgReadedHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila, chat_id
+app.post("/msgReadedHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila, chat_id
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookmsgReadedHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookmsgReadedHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/msgSentHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila
+app.post("/msgSentHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookmsgSentHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookmsgSentHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/msgDeletedHook", (req, res, next)=>{
-    const data= req.body;
-    console.log(data);
-    //Data= instancia, fila
+app.post("/msgDeletedHook", (req, res, next) => {
+  const data = req.body;
+  console.log(data);
+  //Data= instancia, fila
 
-    if(validarDados(data)){
-        const room= determinarSala(data);
-        if(room){
-            io.to(room).emit("webhookmsgDeletedHook", data);
-            res.status(200).send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
-        }else{
-            res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`)
-        }
-    }else {
-        res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  if (validarDados(data)) {
+    const room = determinarSala(data);
+    if (room) {
+      io.to(room).emit("webhookmsgDeletedHook", data);
+      res
+        .status(200)
+        .send(`Webhook recebido com sucesso. Enviado para sala: ${room}`);
+    } else {
+      res.status(400).send(`Erro ao determinar a sala correspondente. ${room}`);
     }
-})
+  } else {
+    res.status(400).send(`Dados do webhook inválidos. ${data}`);
+  }
+});
 
-app.post("/", (req, res, next)=>{
-    res.send("Hello World!");
-})
-
-
+app.post("/", (req, res, next) => {
+  res.send("Hello World!");
+});
 
 //Função para validação de dados do Webhook:
-function validarDados(data){
-    return (typeof data.instancia === 'string' && !isNaN(data.fila));
+function validarDados(data) {
+  return typeof data.instancia === "string" && !isNaN(data.fila);
 }
 
 //Função para determinar sala do webhook:
-function determinarSala(data){
-    console.log(data)
-    return `${data.instancia}_${data.fila}`
+function determinarSala(data) {
+  console.log(data);
+  return `${data.instancia}_${data.fila}`;
 }
 
 const PORT = process.env.PORT || 3004;
 server.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
-    console.log(`Socket.IO endpoint: http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
+  console.log(`Socket.IO endpoint: http://localhost:${PORT}`);
 });
